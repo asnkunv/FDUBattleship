243667070c6befb978964c4479a7c2efbf4714a8
Emirlan <windy33e@gmail.com>
2026-03-28 20:27:34 -0400

    Test commit

--- src/main/java/org/fdu/App.java
+++ src/main/java/org/fdu/App.java
@@ -2,6 +2,7 @@ package org.fdu;
 
 public class App {
     public static void main(String[] args) {
+        //Test
         BattleBoard board = new BattleBoard();
         board.displayBoard();
     }
