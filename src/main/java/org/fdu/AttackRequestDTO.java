ce19eefa5500e70858a7124b22b1146fb06db9e1
Emirlan Asanakunov <windy33e@gmail.com>
2026-05-03 19:07:56 -0400

    Modify comment in AttackRequestDTO
    Updated comment to clarify the purpose of the DTO.

--- src/main/java/org/fdu/AttackRequestDTO.java
+++ src/main/java/org/fdu/AttackRequestDTO.java
@@ -4,7 +4,7 @@ package org.fdu;
  * DTO representing an incoming attack request from the frontend.
  * @param row The 0-indexed row coordinate of the targeted cell (0-9).
  * @param column The 0-indexed column coordinate of the targeted cell (0-9).
- * Testingdw
+ * Testingdw something
  */
 
 public record AttackRequestDTO(int row, int column) {
