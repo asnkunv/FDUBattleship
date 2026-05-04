eb733583c1c3be90fa065404d5d991cd307bbb29
Emirlan Asanakunov <windy33e@gmail.com>
2026-05-03 20:10:30 -0400

    Update AttackRequestDTO.java

--- src/main/java/org/fdu/AttackRequestDTO.java
+++ src/main/java/org/fdu/AttackRequestDTO.java
@@ -3,7 +3,7 @@ package org.fdu;
 /**
  * DTO representing an incoming attack request from the frontend.
  * @param row The 0-indexed row coordinate of the targeted cell (0-9).
- * @param column The 0-indexed column coordinate of the targeted cell (0-9).
+ * @param column The 0-indexed column coordinate of the targeted cell (0-9).fes
  * Testingdw something
  */
 
